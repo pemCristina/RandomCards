3d227951ea59c490f8db4d9177a1c5628ccfb1b5
Cristina Pérez <pem.cristina@gmail.com>
2017-02-19 13:43:08 +0100

    Firt commit

---
+++ app/src/main/java/com/android/ejemplos/randomcards/DeckActivity.java
@@ -0,0 +1,13 @@
+package com.android.ejemplos.randomcards;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DeckActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_deck);
+    }
+}
