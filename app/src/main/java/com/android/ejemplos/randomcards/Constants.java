02cc2556418dafa5a5af72fd839e6d26cc0a509c
Cristina Pérez <pem.cristina@gmail.com>
2017-02-24 17:22:07 +0100

    Added Constants class

---
+++ app/src/main/java/com/android/ejemplos/randomcards/Constants.java
@@ -0,0 +1,7 @@
+package com.android.ejemplos.randomcards;
+
+public class Constants {
+    public static final String CREATE_DECK_URL = "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1";
+    public static final String GET_CARD_BASE_URL_FIRST = "https://deckofcardsapi.com/api/deck/";
+    public static final String GET_CARD_BASE_URL_LAST = "/draw/?count=1";
+}
